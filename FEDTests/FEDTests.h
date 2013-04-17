fa02c6d6834dc4c37f13095b22febfad0aa870a2
Yan Rabovik <yan@rabovik.ru>
2013-04-17 12:04:51 +0400

    project template

---
+++ FEDTests/FEDTests.h
@@ -0,0 +1,13 @@
+//
+//  FEDTests.h
+//  FEDTests
+//
+//  Created by Yan Rabovik on 17.04.13.
+//  Copyright (c) 2013 Yan Rabovik. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FEDTests : SenTestCase
+
+@end
---
+++ FEDTests/FEDTests.m
@@ -0,0 +1,32 @@
+//
+//  FEDTests.m
+//  FEDTests
+//
+//  Created by Yan Rabovik on 17.04.13.
+//  Copyright (c) 2013 Yan Rabovik. All rights reserved.
+//
+
+#import "FEDTests.h"
+
+@implementation FEDTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FEDTests");
+}
+
+@end
