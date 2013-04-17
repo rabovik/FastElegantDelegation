53d4008c372ebdbbb74f644fbb5b0a67795f137c
Yan Rabovik <yan@rabovik.ru>
2013-04-17 16:11:09 +0400

    basic forwarding

--- FEDelegation/Proxy/FEDProxy.mm
+++ FEDelegation/Proxy/FEDProxy.mm
@@ -11,12 +11,16 @@
 #import "RTMethod.h"
 #import "RTProtocol.h"
 #import <unordered_map>
+#import <unordered_set>
 
 @implementation FEDProxy{
+    __weak id _delegate;
     Protocol *_protocol;
     std::unordered_map<SEL,id> _signatures;
+    std::unordered_set<SEL> _delegateSelectors;
 }
 
+#pragma mark - Init
 +(Class)proxyClass{
     return self;
 }
@@ -28,16 +32,32 @@
 }
 
 -(id)fed_initWithDelegate:(id)delegate protocol:(Protocol *)protocol{
+    _delegate = delegate;
     _protocol = protocol;
-    
     NSArray *methods = [FEDUtils instanceMethodsInProtocol:protocol withAdopted:YES];
     for (RTMethod *method in methods){
+        SEL selector = method.selector;
+        
+        if ([_delegate respondsToSelector:selector]) {
+            _delegateSelectors.insert(selector);
+        }
         const char *types = [method.signature
                              cStringUsingEncoding:NSUTF8StringEncoding];
-        _signatures[method.selector] = [NSMethodSignature signatureWithObjCTypes:types];
+        _signatures[selector] = [NSMethodSignature signatureWithObjCTypes:types];
     }
-    
     return self;
+}
+
+#pragma mark - Forwarding
+-(id)forwardingTargetForSelector:(SEL)selector{
+    std::unordered_set<SEL>::const_iterator pair = _delegateSelectors.find(selector);
+    if (pair != _delegateSelectors.end()) {
+        id strongDelegate = _delegate;
+        if (strongDelegate) {
+            return strongDelegate;
+        }
+    }
+    return nil;
 }
 
 -(NSMethodSignature *)methodSignatureForSelector:(SEL)selector{
@@ -45,9 +65,19 @@
     if (pair != _signatures.end()) {
         return pair->second;
     }else{
-        return [super methodSignatureForSelector:selector];
+        @throw [NSException
+                exceptionWithName:@"FEDProxyException"
+                reason:[NSString stringWithFormat:
+                        @"No method signature for selector %@ in protocol %@",
+                        NSStringFromSelector(selector),
+                        NSStringFromProtocol(_protocol)]
+                userInfo:nil];
     }
     return nil;
+}
+
+-(void)forwardInvocation:(NSInvocation *)invocation{
+    [invocation invokeWithTarget:nil];
 }
 
 @end
