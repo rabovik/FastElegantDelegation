6c8302134c8bf8ec6471f521119f36aa517ba3eb
Yan Rabovik <yan@rabovik.ru>
2013-04-19 17:41:59 +0400

    Basic multiProxy functionality

--- FEDelegation/MultiProxy/FEDMultiProxy.mm
+++ FEDelegation/MultiProxy/FEDMultiProxy.mm
@@ -8,12 +8,18 @@
 
 #import "FEDMultiProxy.h"
 #import "FEDRuntime.h"
+#import "RTProtocol.h"
+#import "MAObjCRuntime+FEDAdditions.h"
+#import "RTMethod.h"
 #import <vector>
+#import <unordered_map>
 
 #define FED_MULTIPROXY_IVARS                                                             \
     Protocol *_protocol;                                                                 \
     dispatch_block_t _onDeallocBlock;                                                    \
-    std::vector<__weak id> _delegates;
+    std::vector<__weak id> _delegates;                                                   \
+    NSMutableArray *_strongDelegates;                                                    \
+    std::unordered_map<SEL,id> _signatures;
 
 #pragma mark - IOS 5 HACK -
 #if __IPHONE_OS_VERSION_MIN_REQUIRED < 60000
@@ -94,21 +100,40 @@
 {
     _protocol = protocol;
     
-    // ... delegates
     for (id delegate in delegates) {
         _delegates.push_back(delegate);
     }
     
+    [self fed_prepareSelectorsCache];
+    
     if (retainDelegates) {
-        // ...
+        _strongDelegates = [NSMutableArray arrayWithArray:delegates];
     }
     
     if (retainedByDelegates) {
-        // ...
+        for (id delegate in delegates) {
+            objc_setAssociatedObject(delegate, (__bridge void *)self, self, OBJC_ASSOCIATION_RETAIN);
+        }
     }
     
     _onDeallocBlock = [block copy];
 }
+
+-(void)fed_prepareSelectorsCache{
+    RTProtocol *protocol = [RTProtocol protocolWithObjCProtocol:_protocol];
+    NSArray *requiredMethods = [protocol methodsRequired:YES
+                                                instance:YES
+                                            incorporated:YES];
+    NSArray *optionalMethods = [protocol methodsRequired:NO
+                                                instance:YES
+                                            incorporated:YES];
+    NSArray *allMethods = [requiredMethods arrayByAddingObjectsFromArray:optionalMethods];
+    for (RTMethod *method in allMethods){
+        _signatures[method.selector] =
+            [NSMethodSignature signatureWithObjCTypes:method.signature.UTF8String];
+    }
+}
+
 
 -(void)dealloc{
     if (_onDeallocBlock) _onDeallocBlock();
@@ -130,6 +155,42 @@
                                     }),
                      _delegates.end());
     return array;
+}
+
+#pragma mark - Forwarding
+-(NSMethodSignature *)methodSignatureForSelector:(SEL)selector{
+    auto pair = _signatures.find(selector);
+    if (pair != _signatures.end()) {
+        return pair->second;
+    }else{
+        @throw [NSException
+                exceptionWithName:@"FEDMultiProxyException"
+                reason:[NSString stringWithFormat:
+                        @"No method signature for selector %@ in protocol %@",
+                        NSStringFromSelector(selector),
+                        NSStringFromProtocol(_protocol)]
+                userInfo:nil];
+    }
+    return nil;
+}
+
+-(void)forwardInvocation:(NSInvocation *)invocation{
+    BOOL voidReturnType = (0 == strcmp("v", invocation.methodSignature.methodReturnType));
+    for (id delegate in self.fed_realDelegates) {
+        if ([delegate respondsToSelector:invocation.selector]) {
+            [invocation invokeWithTarget:delegate];
+            if (!voidReturnType) break;
+        }
+    }
+}
+
+-(BOOL)respondsToSelector:(SEL)selector{
+    for (id delegate in self.fed_realDelegates) {
+        if ([delegate respondsToSelector:selector]) {
+            return YES;
+        }
+    }
+    return NO;
 }
 
 
